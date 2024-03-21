2d2058cd5004b9e096dc5144f875992e7867c85b
khoihoccodepy <ddkhoi.dhkl17a2hn@sv.uneti.edu.vn>
2024-03-22 05:39:23 +0700

    Create lap2.3.py

---
+++ btvnlap2/lap2.3.py
@@ -0,0 +1,205 @@
+#bai 3 cô oi! bài này nếu chỉ sử dụng các kiến thức của 2 bài học trước thì với cái đầu đất của em chỉ không nghĩ ra được cách nào tối ưu hơn nên em xin phép cô em chỉ sử làm đến 2 chữ số thôi ạ mong sư phụ tha thứ cho sự hồ đồ này của đệ tử!!!
+
+n = int(input("nhap so nguyen :"))
+if n == 0:
+    print("zero")
+elif n == 1 :
+    print("one")
+elif n == 2 :
+    print("two")
+elif n == 3 :
+    print("three") 
+elif n == 4 :
+    print("four")
+elif n == 5 :
+    print("five")
+elif n == 6 :
+    print("six")
+elif n == 7 :
+    print("seven")
+elif n == 8 :
+    print("eight")
+elif n == 9 :
+    print("nine") 
+elif n == 10 :
+    print("ten")
+elif n == 11 :
+    print("eleven")
+elif n == 12 :
+    print("twelve")
+elif n == 13 :
+    print("thirteen")
+elif n == 14 :
+    print("fourteen")
+elif n == 15 :
+    print("fifteen") 
+elif n == 16 :
+    print("sixteen")
+elif n == 17 :
+    print("seventeen")
+elif n == 18 :
+    print("eighteen")
+elif n == 19 :
+    print("nineteen")
+elif n == 20 :
+    print("twenty")
+elif n == 21:
+    print("twentyone")
+elif n == 22 :
+    print("twentytwo")
+elif n == 23 :
+    print("twentythree") 
+elif n == 24 :
+    print("twentyfour")
+elif n == 25 :
+    print("twentyfive")
+elif n == 26 :
+    print("twentysix")
+elif n == 27 :
+    print("twentyseven")
+elif n == 28 :
+    print("twentyeight")
+elif n == 29 :
+    print("twentynine")
+elif n == 30 :
+    print("thirty")
+elif n == 31 :
+    print("thirtone")
+elif n == 32 :
+    print("thirttwo")
+elif n == 33 :
+    print("thirtthree") 
+elif n == 34 :
+    print("thirtfour")
+elif n == 35 :
+    print("thirtfive")
+elif n == 36 :
+    print("thirtsix")
+elif n == 37 :
+    print("thirtseven")
+elif n == 38 :
+    print("thirteight")
+elif n == 39 :
+    print("thirtnine")
+elif n == 40:
+    print("forty")
+elif n == 41 :
+    print("fortyone")
+elif n == 42 :
+    print("fortytwo")
+elif n == 43 :
+    print("fortythree") 
+elif n == 44 :
+    print("fortyfour")
+elif n == 45 :
+    print("fortyfive")
+elif n == 46 :
+    print("fortysix")
+elif n == 47 :
+    print("fortyseven")
+elif n == 48 :
+    print("fortyeight")
+elif n == 49 :
+    print("fortynine")
+elif n == 50:
+    print("fifty")
+elif n == 51 :
+    print("fiftyone")
+elif n == 52 :
+    print("fiftytwo")
+elif n == 53 :
+    print("fiftythree") 
+elif n == 54 :
+    print("fiftyfour")
+elif n == 55 :
+    print("fiftyfive")
+elif n == 56 :
+    print("fiftysix")
+elif n == 57 :
+    print("fiftyseven")
+elif n == 58 :
+    print("fiftyeight")
+elif n == 59 :
+    print("fiftynine")
+elif n == 60:
+    print("sixty")
+elif n == 61 :
+    print("sixtynone")
+elif n == 62 :
+    print("sixtytwo")
+elif n == 63 :
+    print("sixtythree") 
+elif n == 64 :
+    print("sixtyfour")
+elif n == 65 :
+    print("sixtyfive")
+elif n == 66 :
+    print("sixtysix")
+elif n == 67 :
+    print("sixtyseven")
+elif n == 68 :
+    print("sixtyeight")
+elif n == 69 :
+    print("sixtynine")
+elif n == 70 :
+    print("seventy")
+elif n == 71 :
+    print("seventyone")
+elif n == 72 :
+    print("seventytwo")
+elif n == 73 :
+    print("seventythree") 
+elif n == 74 :
+    print("seventyfour")
+elif n == 75 :
+    print("seventyfive")
+elif n == 76 :
+    print("seventysix")
+elif n == 77 :
+    print("seventyseven")
+elif n == 78 :
+    print("seventyeight")
+elif n == 79 :
+    print("seventynine")
+elif n == 80 :
+    print("eighty")
+elif n == 81 :
+    print("eightyone")
+elif n == 82 :
+    print("eightytwo")
+elif n == 83 :
+    print("eightythree") 
+elif n == 84 :
+    print("eightyfour")
+elif n == 85 :
+    print("eightyfive")
+elif n == 86 :
+    print("eightysix")
+elif n == 87 :
+    print("eightyseven")
+elif n == 88 :
+    print("eightyeight")
+elif n == 89 :
+    print("eightynine") 
+elif n == 90 :
+    print("ninety")
+elif n == 91 :
+    print("ninetyone")
+elif n == 92 :
+    print("ninetytwo")
+elif n == 93 :
+    print("ninetythree") 
+elif n == 94 :
+    print("ninetyfour")
+elif n == 95 :
+    print("ninetyfive")
+elif n == 96 :
+    print("ninetysix")
+elif n == 97 :
+    print("ninetyseven")
+elif n == 98 :
+    print("ninetyeight")
+elif n == 99 :
+    print("ninetynine") 
+elif n == 100 :
+    print("one hundred")
