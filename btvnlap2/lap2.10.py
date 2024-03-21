4f786617c072e9d041980a466a7ab027edf15fa4
khoihoccodepy <ddkhoi.dhkl17a2hn@sv.uneti.edu.vn>
2024-03-22 05:38:52 +0700

    Create lap2.10.py

---
+++ btvnlap2/lap2.10.py
@@ -0,0 +1,48 @@
+print("wellcome to khoihoccode!!!!!!!")
+tl = input("Nhập thể loại phim bạn muốn xem(Hành động, Kinh dị, Tình cảm, Hài hước, Hoạt hình):")
+tg =input("Nhập thời gian muốn xem phim (sáng, trưa, chiều, tối):")
+if tl == "Hành động":
+    if tg == "sáng":
+        print("có suất chiếu vào thời gian này")
+    elif tg == "trưa":
+        print("Không có suất chiếu vào thời gian này ")
+    elif tg == "chiều":
+        print("Không có suất chiếu vào thời gian này ")
+    elif tg == "tối":
+        print("có suất chiếu vào thời gian này")
+elif tl == "Kinh dị":
+    if tg == "sáng":
+        print("Không có suất chiếu vào thời gian này")
+    elif tg == "trưa":
+        print(" có suất chiếu vào thời gian này ")
+    elif tg == "chiều":
+        print("Không có suất chiếu vào thời gian này ")
+    elif tg == "tối":
+        print("có suất chiếu vào thời gian này")
+elif tl == "Tình cảm":
+    if tg == "sáng":
+        print(" có suất chiếu vào thời gian này")
+    elif tg == "trưa":
+        print(" có suất chiếu vào thời gian này ")
+    elif tg == "chiều":
+        print(" có suất chiếu vào thời gian này ")
+    elif tg == "tối":
+        print("có suất chiếu vào thời gian này")
+elif tl == "Hài hước":
+    if tg == "sáng":
+        print(" có suất chiếu vào thời gian này")
+    elif tg == "trưa":
+        print(" Không có suất chiếu vào thời gian này ")
+    elif tg == "chiều":
+        print(" Không có suất chiếu vào thời gian này ")
+    elif tg == "tối":
+        print("có suất chiếu vào thời gian này")  
+elif tl == "Hoạt hình":
+    if tg == "sáng":
+        print(" có suất chiếu vào thời gian này")
+    elif tg == "trưa":
+        print(" Không có suất chiếu vào thời gian này ")
+    elif tg == "chiều":
+        print(" có suất chiếu vào thời gian này ")
+    elif tg == "tối":
+        print("có suất chiếu vào thời gian này")
