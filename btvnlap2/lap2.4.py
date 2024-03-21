4a3b20c1cce8758deb2512723833bb9fdb8aeff1
khoihoccodepy <ddkhoi.dhkl17a2hn@sv.uneti.edu.vn>
2024-03-22 05:40:13 +0700

    Create lap2.4.py

---
+++ btvnlap2/lap2.4.py
@@ -0,0 +1,13 @@
+n = float(input("nhap diem :"))
+if 0 <= n <= 5:
+    print("Điểm kém")
+elif 5 < n <= 7:
+    print("Điểm trung bình")
+elif 7 < n <= 8.5:
+    print("Điểm khá")
+elif 8.5< n <= 10:
+    print("Điểm tốt")
+else:
+    print("Không hợp lệ")
+
+
