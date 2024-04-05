b570f3e36d5840294c99e9cb7fc6a1de6bf219b3
khoihoccodepy <ddkhoi.dhkl17a2hn@sv.uneti.edu.vn>
2024-04-05 09:31:25 +0700

    Create lap4.6.py

---
+++ btvn lap4/lap4.6.py
@@ -0,0 +1,24 @@
+
+n = input("Nhập số: ")
+print("Kết quả :", end=" ")
+for i in n:
+    if i== '0':
+        print("zero", end=" ")
+    elif i == '1':
+        print("one", end=" ")
+    elif i == '2':
+        print("two", end=" ")
+    elif i == '3':
+        print("three", end=" ")
+    elif i == '4':
+        print("four", end=" ")
+    elif i == '5':
+        print("five", end=" ")
+    elif i == '6':
+        print("six", end=" ")
+    elif i== '7':
+        print("seven", end=" ")
+    elif i == '8':
+        print("eight", end=" ")
+    elif i == '9':
+        print("nine", end=" ")
