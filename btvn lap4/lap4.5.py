a001db8c9d181eac8f0f886d02ee29aace6d7db6
khoihoccodepy <ddkhoi.dhkl17a2hn@sv.uneti.edu.vn>
2024-04-05 09:31:21 +0700

    Create lap4.5.py

---
+++ btvn lap4/lap4.5.py
@@ -0,0 +1,45 @@
+import math
+
+while True:
+    n1 = float(input("Nhập số thứ nhất: "))
+    n2 = float(input("Nhập số thứ hai: "))
+    
+    print("1. Cộng")
+    print("2. Trừ")
+    print("3. Nhân")
+    print("4. Chia")
+    print("5. Lũy thừa")
+    print("6. Căn bậc hai")
+    
+    c = int(input("Chọn phép tính (1-6): "))
+    
+    if c == 1:
+        i = n1 + n2
+        print("Kết quả của", n1, "+", n2, "là:", i)
+    elif c == 2:
+        i = n1 - n2
+        print("Kết quả của", n1, "-", n2, "là:", i)
+    elif c == 3:
+        i = n1 * n2
+        print("Kết quả của", n1, "*", n2, "là:", i)
+    elif c == 4:
+        if n2 != 0:
+            i = n1 / n2
+            print("Kết quả của", n1, "/", n2, "là:", i)
+        else:
+            print("Lỗi.")
+    elif c == 5:
+        i = n1 ** n2
+        print("Kết quả của", n1, "^", n2, "là:", i)
+    elif c == 6:
+        if n2 >= 0:
+            r = math.sqrt(n2)
+            print("Căn bậc hai của", n2, "là:", r)
+        else:
+            print("Lỗi")
+    else:
+        print("Lỗi.")
+        
+    continue_choice = input("tiếp tục không? (c/k): ")
+    if continue_choice.lower() != 'c':
+        break
